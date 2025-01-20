903d77dd044ad6e7ef668ff3098bcfef3fe620b8
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-20 16:41:43 +0100

    feat: add server with express and images folder

---
+++ app.js
@@ -0,0 +1,13 @@
+// Local server with express.js
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
