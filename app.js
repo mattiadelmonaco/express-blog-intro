9612d696697096a6d49c62a8146689e2c5cd7e38
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-20 16:54:09 +0100

    feat: add route ("/bacheca")

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
 
+app.get("/bacheca", (req, res) => {
+    res.json(posts)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
@@ -45,6 +49,4 @@ const posts = [
       image: "torta_paesana.jpg",
       tags: ["dolci", "tradizione", "torta paesana", "Lombardia"]
     }
-  ];
-  
-  console.log(posts);
+  ]
