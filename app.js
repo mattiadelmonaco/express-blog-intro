1d9f8d18e38fcb42823118380ae0a04ea3ac2c9f
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-01-20 17:05:12 +0100

    feat: add static folder (public), move images inside public, set link in array to show images

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
@@ -22,31 +24,33 @@ const posts = [
     {
       title: "Il segreto del perfetto Ciambellone",
       content: "Un dolce classico della tradizione, semplice da preparare ma sempre delizioso. Scopri la ricetta per un ciambellone soffice e profumato.",
-      image: "ciambellone.jpg",
+      image: "http://localhost:3000/images/ciambellone.jpeg",
       tags: ["dolci", "tradizione", "ciambellone", "ricette facili"]
     },
     {
       title: "Cracker alla Barbabietola: salute e gusto",
       content: "Un'idea originale e sana per uno snack croccante e colorato. Ecco come preparare dei cracker alla barbabietola fatti in casa.",
-      image: "cracker_barbabietola.jpg",
+      image: "http://localhost:3000/images/cracker_barbabietola.jpeg",
       tags: ["snack", "barbabietola", "healthy", "ricette vegane"]
     },
     {
       title: "Pane Fritto Dolce: la bontà che stupisce",
       content: "Un piatto sorprendente, croccante all'esterno e morbido dentro. Perfetto per la colazione o uno sfizio pomeridiano.",
-      image: "pane_fritto_dolce.jpg",
+      image: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
       tags: ["pane", "dolci fritti", "colazione", "sfiziosità"]
     },
     {
       title: "Pasta alla Barbabietola: un'esplosione di colori",
       content: "Trasforma un piatto tradizionale con l'aggiunta del colore vibrante della barbabietola. Una ricetta semplice e scenografica.",
-      image: "pasta_barbabietola.jpg",
+      image: "http://localhost:3000/images/pasta_barbabietola.jpeg",
       tags: ["pasta", "barbabietola", "vegetariano", "creativo"]
     },
     {
       title: "Torta Paesana: sapori di una volta",
       content: "Un dolce della tradizione lombarda che celebra i sapori genuini di una volta. Ecco la ricetta passo passo.",
-      image: "torta_paesana.jpg",
+      image: "http://localhost:3000/images/torta_paesana.jpeg",
       tags: ["dolci", "tradizione", "torta paesana", "Lombardia"]
     }
   ]
+
+  console.log(posts)
